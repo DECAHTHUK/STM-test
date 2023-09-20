ef6e13052e56b2438a419dfea520283ec3d72096
DECAHTHUK <alexei.talanov003@mail.ru>
2023-09-20 19:21:07 +0300

    Feat: added tests for UserController

---
+++ src/test/java/org/example/MainTests.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MainTests {
+    @Test
+    void contextLoads() {
+    }
+}
